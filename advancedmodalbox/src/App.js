d3a51b4a20674ca0015302e2877a8d16a3ca4719
VikasWilliam <vikasdenobili@outlook.com>
2022-07-20 10:31:19 +0530

    header added

--- advancedmodalbox/src/App.js
+++ advancedmodalbox/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+const component_Style={
+  position: 'relative',
+  zIndex:2,
+  backgroundColor:'red',
+  padding:'10px'
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={component_Style}>
+      <h1>Welcome to Advanced Modal Box tutorial</h1>
     </div>
   );
 }
