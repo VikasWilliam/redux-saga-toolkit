94958113640b3d45d77797f1d931095c2f6d0c57
VikasWilliam <vikasdenobili@outlook.com>
2022-08-05 12:45:07 +0530

    form button 8615

--- formicform/src/form.js
+++ formicform/src/form.js
@@ -20,6 +20,17 @@ const FormDemo= () => {
                     <input type="radio" value="female"></input>
                     <label>female</label>
                 </div>
+                <div>
+                    <p>Address:</p>
+                    <input type="text"></input>
+                </div>
+                <div>
+                    <p>address:</p>
+                    <input type="password"></input>
+                </div>
+                <div>
+                    <button>Submit</button>
+                </div>
             </form>
         </>
         )
